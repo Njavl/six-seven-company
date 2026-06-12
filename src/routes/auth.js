728bd9d4b694e677a0b8366c797cff4df95519b4
Tanya <yakovetsta1@gmail.com>
2026-06-12 11:02:17 -0300

    auth-register

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,11 @@
 import { Router } from 'express';
 
+import { registerUserController } from '../controllers/auth.js';
+import { registerUserSchema } from '../validation/auth.js';
+import { validateBody } from '../middlewares/validatebody.js';
+
 const router = Router();
+
+router.post('/register', validateBody(registerUserSchema), registerUserController);
 
 export default router;
