80c0b1d5ef2ecbaf20f19e9852c8c6e0f0e64baa
olenakolesnik <lelya6350@gmail.com>
2026-06-15 19:09:31 +0300

    add categories endpoint (#20)
    * add categories endpoint
    
    * fixed controller

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import { getAllCategoriesController } from '../controllers/categories.js';
 
 const router = Router();
+
+router.get('/', getAllCategoriesController);
 
 export default router;
